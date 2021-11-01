88cceda7232e9bdfac939c44f75179671d77e809
HOON\shonn <shonn2323@gmail.com>
2021-11-01 20:18:21 +0900

    사이드 메뉴바 추가

--- src/ac/injecs/java2/Main.java
+++ src/ac/injecs/java2/Main.java
@@ -9,13 +9,15 @@ public class Main extends JFrame {
     public Main(){
         setTitle("인제 클래스 매니저");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(300, 200);
 
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
 
-        add(new MenuBar());
+        // 하위 패널 추가
+        add(BorderLayout.WEST,new MenuBar());
 
-        setSize(300, 200);
         setVisible(true);
     }
     public static void main(String[] args) {
