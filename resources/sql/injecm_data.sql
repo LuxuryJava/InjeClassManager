7b23acb2ffeb4379c981d10ac05b11f386466071
min-0 <dhe08133@naver.com>
2021-11-30 22:01:04 +0900

    createdate 추가 및 notification DB 수정

--- resources/sql/injecm_data.sql
+++ resources/sql/injecm_data.sql
@@ -14,11 +14,10 @@ values  ('금'),
         ('토'),
         ('화');
 
-INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('[2022학년도 1학기 제2전공 관련 안내]
-', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD/?uid=3308&mod=document&pageid=1');
-INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('[USG공유대학 학생 모집]
-', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD/?uid=3359&mod=document&pageid=1');
-INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('2021-2 기말고사 시간표 안내
+INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('<특강> 옥재 윤상길의 "가야 그리고 영혼의 불꽃" 도예展', 'https://navi.inje.ac.kr/program/E/ProgE0011S.aspx?yy=2021&smt=2&code=000000084&dept=01564&no=2&mc=0152');
+INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('<공지사항> 2021-2 기말고사 시간표 안내
 ', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD/?uid=3465&mod=document&pageid=1');
-INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('ICT InnoFesta 2021
-', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%+95%AD/?uid=3358&mod=document&pageid=1');
+INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('<공지사항> [2022학년도 1학기 제2전공 관련 안내]
+', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD/?uid=3308&mod=document&pageid=1');
+INSERT INTO injecm.notification (ntitle, ncontent) VALUES ('<공지사항> [USG공유대학 학생 모집]
+', 'https://cs.inje.ac.kr/%EA%B3%B5%EC%A7%80%EC%82%AC%ED%95%AD/?uid=3359&mod=document&pageid=1');
