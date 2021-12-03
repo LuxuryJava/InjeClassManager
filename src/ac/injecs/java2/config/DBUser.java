f1e5160d608da7d9f4513d532fb4b07d9e5be3d1
HOON\shonn <shonn2323@gmail.com>
2021-12-03 21:36:23 +0900

    [#1] 강의실 예약 시 SQL 이슈 - Reservation -> mencnt에 오류난 부분 수정

--- src/ac/injecs/java2/config/DBUser.java
+++ src/ac/injecs/java2/config/DBUser.java
@@ -1,11 +1,6 @@
 package ac.injecs.java2.config;
 
 public class DBUser {
-<<<<<<< HEAD
     static final String USER = "inje";
     static final String PASSWORD = "inje";
-=======
-    static final String USER = "root";
-    static final String PASSWORD = "narin516";
->>>>>>> ad0ffc8af63ae23ace7e48015f37f05d19219eff
 }
