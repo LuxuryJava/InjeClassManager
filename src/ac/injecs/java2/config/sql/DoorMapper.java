a9086db37b20c00179bfc2c63d12e9c7b6a2390a
min-0 <dhe08133@naver.com>
2021-12-03 22:22:52 +0900

    메세지 박스 수정

--- src/ac/injecs/java2/config/sql/DoorMapper.java
+++ src/ac/injecs/java2/config/sql/DoorMapper.java
@@ -2,14 +2,10 @@ package ac.injecs.java2.config.sql;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
 
 import javax.swing.JOptionPane;
 
 import ac.injecs.java2.entity.Door;
-import ac.injecs.java2.entity.Room;
 
 public class DoorMapper extends SQLMapper {
 
@@ -27,49 +23,13 @@ public class DoorMapper extends SQLMapper {
             int row = preparedStatement.executeUpdate();
 
             System.out.println("추가된 row : " + row);
-<<<<<<< HEAD
             JOptionPane.showMessageDialog(null,"개방/잠금 요청 되었습니다.");
-
         } catch (SQLException e) {
             //e.printStackTrace();
             JOptionPane.showMessageDialog(null,"이미 요청되었습니다.");
-=======
-            JOptionPane .showMessageDialog(null,"개방/잠금 요청 되었습니다.");
-
-        } catch (SQLException e) {
-            //e.printStackTrace();
-            JOptionPane .showMessageDialog(null,"이미 요청되었습니다.");
->>>>>>> c7dd3551253cfc9133d169f34edfb684c28f02e9
         }
 
 	}
-
-//	@Override
-//	public Object select(PreparedStatement preparedStatement) {
-//		Vector<Object> doors = new Vector<Object>();
-//		
-//        try {
-//            this.preparedStatement = preparedStatement;
-//            this.resultSet = preparedStatement.executeQuery();
-//
-//            while (resultSet.next()) {
-//            	doors.add(
-//                        (Object)new Door.Builder()
-//                                .setuno(resultSet.getString("uno"))
-//                                .setrinfo(resultSet.getString("rinfo"))
-//                                .setdoorOpen(resultSet.getBoolean("doorOpen"))
-//                                .build()
-//                );
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        if(doors.size() == 1){
-//            return doors.get(0);
-//        }
-//        return doors;
-//	}
 
 	@Override
 	public void update(PreparedStatement preparedStatement, Object object) {
@@ -103,7 +63,7 @@ public class DoorMapper extends SQLMapper {
 			preparedStatement.setString(1, door.getrinfo());
 			
 			int row = preparedStatement.executeUpdate();
-			//JOptionPane.showMessageDialog(null,"개방/잠금 완료");
+			JOptionPane.showMessageDialog(null,"개방/잠금 완료");
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
